2a72f478b58ed5bdbbd2997688db9141e83268f2
Mike Walker <noisufnoc@gmail.com>
2016-03-31 17:47:35 -0400

    better formatting for scaling_freq

--- bin/scaling_freq.sh
+++ bin/scaling_freq.sh
@@ -7,6 +7,6 @@
 # TODO: Find better way to do this
 
 echo -n "\
-	cur_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) \
-	min_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq) \
-	max_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)"
+cur_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) \
+min_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq) \
+max_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)"
